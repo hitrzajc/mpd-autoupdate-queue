bc5293d92e100ee76c3f299858370cf7997b34f3
NightcoreSergeant <tomazic.tadej@gmail.com>
2021-04-28 21:32:57 +0200

    added sudo

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
+sudo
+
 git pull
 make
-cp -f mpd-update-queue /usr/bin/
-cp -f mpd-update-queue.* /etc/systemd/system/
+
+sudo cp -f mpd-update-queue /usr/bin/
+sudo cp -f mpd-update-queue.* /etc/systemd/system/
 
 sudo systemctl enable mpd-update-queue.timer
