ef4dd179b9c54b145a0cdbe3380f48756a2b8c5d
hitrzajc <tomazic.tadej@gmail.com>
2021-05-10 19:20:36 +0200

    Update data.h

--- data.h
+++ data.h
@@ -1,7 +1,7 @@
 #define MPD_HOST "192.168.1.189"
 #define MPD_PORT 6600
 #define MPD_TIMEOUT 1000
-#define MPD_PASSWORD "soca .sdfci aw"
+#define MPD_PASSWORD ""
 
 
 const char *EXCLUDE[] = {".AAA_songs_from_phone",".AAA_long_mix"};
